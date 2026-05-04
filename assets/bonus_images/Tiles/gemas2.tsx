a29f764c3bb234872629ac0af1a88522808b0358
pespinas <77834644+pespinas@users.noreply.github.com>
2026-05-04 17:06:29 +0200

    Add gems prizes

---
+++ assets/bonus_images/Tiles/gemas2.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="GemasP" tilewidth="17" tileheight="16" tilecount="12" columns="6">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="../../../Downloads/Proyecto nuevo (2).png" width="104" height="32"/>
+</tileset>
