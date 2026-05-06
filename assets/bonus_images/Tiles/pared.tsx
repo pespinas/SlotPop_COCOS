c57f05778f751cd42ed4781721066f58cf9e49d3
pespinas <77834644+pespinas@users.noreply.github.com>
2026-05-06 23:05:54 +0200

    Tiles add gem and reimport

--- assets/bonus_images/Tiles/pared.tsx
+++ assets/bonus_images/Tiles/pared.tsx
@@ -1,4 +1,52 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="wall" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="8" columns="4">
  <image source="wall.png" width="69" height="35"/>
+ <tile id="0">
+  <properties>
+   <property name="hardness" type="int" value="1"/>
+   <property name="type" value="FLOOR"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="hardness" type="int" value="2"/>
+   <property name="type" value="SCAR_GROUND"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="hardness" type="int" value="4"/>
+   <property name="type" value="SCAR_ROCK"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="hardness" type="int" value="6"/>
+   <property name="type" value="LV2_ROCK"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="hardness" type="int" value="0"/>
+   <property name="type" value="NOTHING"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="hardness" type="int" value="3"/>
+   <property name="type" value="GROUND"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="hardness" type="int" value="5"/>
+   <property name="type" value="LV1_ROCK"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="hardness" type="int" value="7"/>
+   <property name="type" value="LV3_ROCK"/>
+  </properties>
+ </tile>
 </tileset>
